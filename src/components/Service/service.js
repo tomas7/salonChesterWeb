3260fe57277dce6f0186105ce8b449aa58a13191
Tomas <haverlatom7@gmail.com>
2020-04-22 11:13:30 +0200

    TestingIcons

--- src/components/Service/service.js
+++ src/components/Service/service.js
@@ -4,6 +4,12 @@ import SvgPath from '../svgPath/svgPath'
 import ContentWrapper from '../../HOC/BodyWrapper/bodyWrapper';
 import img_ from '../../images/graphics/photo_icon.png'
 import iconItem from '../../images/Icons/icon.png'
+import iconItem1 from '../../images/Icons/Asset 1.png'
+import iconItem2 from '../../images/Icons/Asset 2.png'
+import iconItem3 from '../../images/Icons/Asset 4.png'
+import iconItem4 from '../../images/Icons/Asset 5.png'
+import iconItem5 from '../../images/Icons/Asset 6.png'
+import iconItem6 from '../../images/Icons/Asset 7.png'
 import PopUp from '../../HOC/PopUp/popUp'
 // import { disableBodyScroll, enableBodyScroll, clearAllBodyScrollLocks } from 'body-scroll-lock';
 
@@ -59,10 +65,12 @@ function Service(props) {
                         <h1>{props.isGrooming ? "Grooming" : "Grooming Course"}</h1>
                         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur rerum quibusdam alias dolores. Ut tempora aut quis accusantium dolores nisi. Voluptates, excepturi? Aperiam, exercitationem voluptas.</p>
                         <div class={myClass.serviceItems}>
-                            <img src={iconItem} alt="Blank"/><span></span><p>Service1</p>
-                            <img src={iconItem} alt="Blank"/><span></span><p>Service1</p>
-                            <img src={iconItem} alt="Blank"/><span></span><p>Service1</p>
-                            <img src={iconItem} alt="Blank"/><span></span><p>Service1</p>
+                            <img src={iconItem1} alt="Blank"/><span></span><p>Service1</p>
+                            <img src={iconItem2} alt="Blank"/><span></span><p>Service1</p>
+                            <img src={iconItem3} alt="Blank"/><span></span><p>Service1</p>
+                            <img src={iconItem4} alt="Blank"/><span></span><p>Service1</p>
+                            <img src={iconItem5} alt="Blank"/><span></span><p>Service1</p>
+                            <img src={iconItem6} alt="Blank"/><span></span><p>Service1</p>
                         </div>
                        
                         <a className="captionStyle" onClick={() => SetModal()}>Book a time</a>
